332417e5de024bbb0b6846920c19a7c64e6dc18d
Oleksandr Romashko <alex.rmshk@gmail.com>
2025-10-18 21:51:18 +0200

    Fix not callable errors for orm mixins

--- src/db/models/mixins.py
+++ src/db/models/mixins.py
@@ -10,10 +10,10 @@ class TimestampMixin:
     """Adds created_at and updated_at timestamp fields, set on the DB server."""
 
     created_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), server_default=func.now()
+        DateTime(timezone=True), server_default=func.now()  # pylint: disable=E1102
     )
     updated_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
-        server_default=func.now(),
-        onupdate=func.now(),
+        server_default=func.now(),  # pylint: disable=E1102
+        onupdate=func.now(),  # pylint: disable=E1102
     )
