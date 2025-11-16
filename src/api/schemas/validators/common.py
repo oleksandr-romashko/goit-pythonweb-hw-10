32a01217eb79799b7a9f0e6d61c2307468bc03a8
Oleksandr Romashko <alex.rmshk@gmail.com>
2025-11-16 22:36:37 +0100

    Fix custom Pydantic validators

--- src/api/schemas/validators/common.py
+++ src/api/schemas/validators/common.py
@@ -8,21 +8,18 @@ from src.api.errors.http_errors import raise_http_400_error
 from src.utils.constants import MESSAGE_ERROR_BAD_REQUEST_EMPTY
 
 
-def at_least_one_field_required_validator(cls: Type[BaseModel]) -> Type[BaseModel]:
-    """
-    Class decorator for Pydantic models.
-    Ensures that at least one field has a non-None value.
-    Returns a new subclass with the validator applied.
-    """
+def at_least_one_field_required_validator(
+    model_cls: Type[BaseModel],
+) -> Type[BaseModel]:
+    """Adds validator ensuring that at least one field is not None."""
 
-    class Wrapper(cls):
-        """Wrapped class with added validation method"""
+    @model_validator(mode="before")
+    def _check(values):
+        if not any(value is not None for value in values.values()):
+            raise_http_400_error(MESSAGE_ERROR_BAD_REQUEST_EMPTY)
+        return values
 
-        @model_validator(mode="before")
-        @classmethod
-        def _check_at_least_one_field(cls, values):
-            if not any(value is not None for value in values.values()):
-                raise_http_400_error(MESSAGE_ERROR_BAD_REQUEST_EMPTY)
-            return values
+    setattr(model_cls, "_check_at_least_one_field", _check)
+    model_cls.model_rebuild(force=True)
 
-    return Wrapper
+    return model_cls
