991f950be6aa604ecf39842ed6073be3d53dbfee
Oleksandr Romashko <alex.rmshk@gmail.com>
2025-12-03 13:08:58 +0100

    Fix logging for dev environment with debug

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -6,9 +6,14 @@ a reusable logger instance for the application.
 """
 
 import logging
+import os
+
+# Read log level from environment
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
+
 
 logging.basicConfig(
-    level=logging.INFO,  # default level
+    level=getattr(logging, LOG_LEVEL, logging.INFO),  # default level
     format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
 )
 logger = logging.getLogger("Application")
