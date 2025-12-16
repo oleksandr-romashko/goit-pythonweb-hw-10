9e7949bce9cb8664c2b6b6c58e5cdb9656fddb6c
Oleksandr Romashko <alex.rmshk@gmail.com>
2025-12-16 04:27:02 +0100

    Fix cache circular dependencies

--- src/providers/cache_providers/__init__.py
+++ src/providers/cache_providers/__init__.py
@@ -1,11 +1,1 @@
 """Cache package exposing cache connection and related utilities."""
-
-from .contact_cache import ContactRedisCacheProvider
-from .contacts_count_cache import ContactsCountUserRedisCacheProvider
-from .user_cache import UserRedisCacheProvider
-
-__all__ = [
-    "ContactRedisCacheProvider",
-    "ContactsCountUserRedisCacheProvider",
-    "UserRedisCacheProvider",
-]
